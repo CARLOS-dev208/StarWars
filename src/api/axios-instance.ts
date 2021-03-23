4b50f55ab5a43e06817f738d5b0f4b562c6c6d30
CARLOS-EDUARDO <cadu.carlos514@gmail.com>
2021-03-23 19:39:58 -0300

    Criação do projeto

---
+++ src/api/axios-instance.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosInstance } from "axios";
+
+const baseURL = "https://swapi.dev/api";
+
+export const getAxiosInstance = (): AxiosInstance => {
+  let axiosInstance = axios.create({ baseURL: baseURL });
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      const { status } = error.response;
+
+      if (status > 399) {
+        console.info("Erro na api. Status:", status);
+      }
+    }
+  );
+  return axiosInstance;
+};
